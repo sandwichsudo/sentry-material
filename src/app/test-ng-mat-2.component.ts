4cf1cb8ee86d356457db186af5bbee7a0b632584
Gilly Ames <gilly.ames@gmail.com>
2016-06-13 23:30:50 +0100

    Created basic material log link.

--- src/app/test-ng-mat-2.component.ts
+++ src/app/test-ng-mat-2.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 
+import {MD_CARD_DIRECTIVES} from '@angular2-material/card';
+import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button';
+import {MdIcon, MdIconRegistry} from '@angular2-material/icon';
+
 @Component({
   moduleId: module.id,
   selector: 'test-ng-mat-2-app',
   templateUrl: 'test-ng-mat-2.component.html',
-  styleUrls: ['test-ng-mat-2.component.css']
+  styleUrls: ['test-ng-mat-2.component.css'],
+  directives: [MD_CARD_DIRECTIVES, MD_BUTTON_DIRECTIVES, MdIcon],
+  providers: [MdIconRegistry]
+
 })
 export class TestNgMat2AppComponent {
-  title = 'test-ng-mat-2 works!';
+  title = 'Sentry';
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { TestNgMat2AppComponent, environment } from './app/';
+import { HTTP_PROVIDERS } from '@angular/http';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(TestNgMat2AppComponent);
-
+bootstrap(TestNgMat2AppComponent,[
+    HTTP_PROVIDERS
+]);
