4d9b3c10f163ce85959a688990a9d9f45e7558b4
Gilly Ames <gilly.ames@gmail.com>
2016-06-14 19:44:21 +0100

    Made the log actions smaller. still need some work. Added button and icon directives to app component for header.

--- src/app/test-ng-mat-2.component.ts
+++ src/app/test-ng-mat-2.component.ts
@@ -7,14 +7,16 @@ import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/route
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LogDetailComponent } from './log-detail/log-detail.component';
 import {MD_SIDENAV_DIRECTIVES} from '@angular2-material/sidenav';
+import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button';
+import {MdIcon, MdIconRegistry} from '@angular2-material/icon';
 
 @Component({
   moduleId: module.id,
   selector: 'test-ng-mat-2-app',
   templateUrl: 'test-ng-mat-2.component.html',
   styleUrls: ['test-ng-mat-2.component.css', 'app.component.css'],
-  directives: [ROUTER_DIRECTIVES, MD_SIDENAV_DIRECTIVES],
-  providers: [
+  directives: [ROUTER_DIRECTIVES, MD_SIDENAV_DIRECTIVES, MD_BUTTON_DIRECTIVES, MdIcon],
+  providers: [MdIconRegistry,
     ROUTER_PROVIDERS,
     LogService]
 
