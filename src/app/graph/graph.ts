f8905dc82699c1bdea06c22a7bfd7c2ae070242c
Gilly Ames <amesg@gb-lt-mj33044.local>
2016-06-26 11:08:58 +0100

    WIP: making graph work

--- src/app/graph/graph.ts
+++ src/app/graph/graph.ts
@@ -12,54 +12,26 @@ import {CHART_DIRECTIVES} from 'ng2-charts/ng2-charts';
 export class LineChartDemoComponent {
   // lineChart
   public lineChartData:Array<any> = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'},
-    {data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C'}
+    {data: [99.4, 99.6, 99.5, 99.5, 99.4, 99.3, 99.2], label: 'Series A'}
   ];
-  public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public lineChartLabels:Array<any> = ['13:51', '13:52', '13:53', '13:54', '13:55', '13:56', '13:57'];
   public lineChartOptions:any = {
     animation: false,
     responsive: true
   };
   public lineChartColours:Array<any> = [
-    { // grey
-      backgroundColor: 'rgba(148,159,177,0.2)',
-      borderColor: 'rgba(148,159,177,1)',
-      pointBackgroundColor: 'rgba(148,159,177,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-    },
-    { // dark grey
-      backgroundColor: 'rgba(77,83,96,0.2)',
-      borderColor: 'rgba(77,83,96,1)',
-      pointBackgroundColor: 'rgba(77,83,96,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(77,83,96,1)'
-    },
-    { // grey
-      backgroundColor: 'rgba(148,159,177,0.2)',
-      borderColor: 'rgba(148,159,177,1)',
-      pointBackgroundColor: 'rgba(148,159,177,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    {
+      backgroundColor: 'rgba(0,0,0,0)',
+      borderColor: '#13dcba',
+      pointBackgroundColor: '#f7fd44',
+      pointBorderColor: '#13dcba',
+      pointHoverBackgroundColor: '#13dcba',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)',
+      defaultFontColor: 'white'
     }
   ];
   public lineChartLegend:boolean = true;
   public lineChartType:string = 'line';
-
-  public randomize():void {
-    let _lineChartData:Array<any> = new Array(this.lineChartData.length);
-    for (let i = 0; i < this.lineChartData.length; i++) {
-      _lineChartData[i] = {data: new Array(this.lineChartData[i].data.length), label: this.lineChartData[i].label};
-      for (let j = 0; j < this.lineChartData[i].data.length; j++) {
-        _lineChartData[i].data[j] = Math.floor((Math.random() * 100) + 1);
-      }
-    }
-    this.lineChartData = _lineChartData;
-  }
 
   // events
   public chartClicked(e:any):void {
--- src/app/in-memory-data.service/in-memory-data.service.ts
+++ src/app/in-memory-data.service/in-memory-data.service.ts
@@ -1,7 +1,7 @@
 export class InMemoryDataService {
     createDb() {
         let logs = [{
-            id: 1,
+            id: 0,
             name: 'FAC-1',
             logUrl: 'ct.googleapis.com/aviator',
             hexid: '64:61:6e:67:65:72:7a:6f:6e:65',
@@ -15,7 +15,7 @@ export class InMemoryDataService {
             priority: 0,
             contactDetails: ['fac1@fail.com', 'aguy@facebook.com', 'a.n.otherguy@facebook.com']
         }, {
-                id: 2,
+                id: 1,
                 name: 'FAC-2',
                 logUrl: 'ct.googleapis.com/pilot',
                 hexid: '64:61:6e:67:65:72:7a:6f:6e:65',
@@ -29,7 +29,7 @@ export class InMemoryDataService {
                 priority: 1,
                 contactDetails: ['fac2@fail.com', 'aguy@facebook.com', 'a.n.otherguy@facebook.com']
             }, {
-                id: 3,
+                id: 2,
                 name: 'FAC-3',
                 logUrl: 'ct.googleapis.com/something',
                 hexid: '64:61:6e:67:65:72:7a:6f:6e:65',
@@ -43,7 +43,7 @@ export class InMemoryDataService {
                 priority: -1,
                 contactDetails: ['fac3@fail.com', 'aguy@facebook.com', 'a.n.otherguy@facebook.com']
             }, {
-                id: 4,
+                id: 3,
                 name: 'TW-1',
                 logUrl: 'ct.googleapis.com/blues',
                 hexid: '4f6e6520776f72642e2e2e4a6566667374657221',
@@ -57,7 +57,7 @@ export class InMemoryDataService {
                 priority: 2,
                 contactDetails: ['tw1@fail.com', 'aguy@twitter.com', 'a.n.otherguy@twitter.com']
             }, {
-                id: 5,
+                id: 4,
                 name: 'INS-1',
                 logUrl: 'ct.googleapis.com/magic',
                 hexid: '64:61:6e:67:65:72:7a:6f:6e:65',
@@ -75,19 +75,3 @@ export class InMemoryDataService {
         return { logs };
     }
 }
-
-/*ct.googleapis.com/aviator
-Base64 Log ID: aPaY+B9kgr46jO65KB1M/HFRXWeT1ETRCmesu09P+8Q=
-Operator: Google
-Started: 2013-09-30
-HTTPS supported: yes
-Contact: google-ct-logs@googlegroups.com
-Chrome inclusion status: Included.
-
-ct.googleapis.com/pilot
-Base64 Log ID: pLkJkLQYWBSHuxOizGdwCjw1mAT5G9+443fNDsgN3BA=
-Operator: Google
-Started: 2013-03-25
-HTTPS supported: yes
-Contact: google-ct-logs@googlegroups.com
-Chrome inclusion status: Included.*/
